50e2a83cb734c419bbc6b6a6e3495f43dc05b157
ali <boyali@gmail.com>
2022-06-09 00:32:54 +0900

    Working on the launcher issues.

--- control/communication_delay_compensator/src/communication_delay_compensator_node.cpp
+++ control/communication_delay_compensator/src/communication_delay_compensator_node.cpp
@@ -14,57 +14,47 @@
 
 #include "communication_delay_compensator_node.hpp"
 
-//namespace autoware
-//{
-//	namespace motion
-//	{
-//		namespace control
-//		{
-//namespace observer
-//{
-
-CommunicationDelayCompensatorNode::CommunicationDelayCompensatorNode(
-		const rclcpp::NodeOptions& node_options)
-		: Node("lateral_controller", node_options)
+namespace autoware::motion::control::observer
 {
-	using std::placeholders::_1;
+	CommunicationDelayCompensatorNode::CommunicationDelayCompensatorNode(
+			const rclcpp::NodeOptions& node_options)
+			: Node("lateral_controller", node_options)
+	{
+		using std::placeholders::_1;
 
-	/* set up ros system */
-	float64_t duration{ 50 };  // ms
-	initTimer(duration);
-}
+		/* set up ros system */
+		float64_t duration{ 50 };  // ms
+		initTimer(duration);
+	}
 
-void CommunicationDelayCompensatorNode::initTimer(float64_t period_s)
-{
-	const auto period_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
-			std::chrono::duration<float64_t>(period_s));
+	void CommunicationDelayCompensatorNode::initTimer(float64_t period_s)
+	{
+		const auto period_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::duration<float64_t>(period_s));
 
-	m_timer_ = rclcpp::create_timer(
-			this, get_clock(), period_ns, std::bind(&CommunicationDelayCompensatorNode::onTimer, this));
-}
+		m_timer_ = rclcpp::create_timer(
+				this, get_clock(), period_ns, std::bind(&CommunicationDelayCompensatorNode::onTimer, this));
+	}
 
-void CommunicationDelayCompensatorNode::onTimer()
-{
-	// RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 1000, "In the delay compensator node ");
+	void CommunicationDelayCompensatorNode::onTimer()
+	{
+		// RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 1000, "In the delay compensator node ");
 
-	// RCLCPP_INFO_THROTTLE(this->get_logger(), clk, 1000, "Hello world");
+		// RCLCPP_INFO_THROTTLE(this->get_logger(), clk, 1000, "Hello world");
 
-	// RCLCPP_ERROR_STREAM_THROTTLE(this->get_logger(), steady_clock, 1000, "Hello World!");
+		// RCLCPP_ERROR_STREAM_THROTTLE(this->get_logger(), steady_clock, 1000, "Hello World!");
 
-	// RCLCPP_ERROR(get_logger(), "Trajectory is invalid!, stop computing.");
+		// RCLCPP_ERROR(get_logger(), "Trajectory is invalid!, stop computing.");
 
-	// RCLCPP_DEBUG(get_logger(), "MPC does not have a QP solver");
+		// RCLCPP_DEBUG(get_logger(), "MPC does not have a QP solver");
 
-	RCLCPP_WARN_THROTTLE(
-			get_logger(), *get_clock(), 1000 /*ms*/,
-			"waiting for vehicle measured steering message ...");
-}
+		RCLCPP_WARN_THROTTLE(
+				get_logger(), *get_clock(), 1000 /*ms*/,
+				"waiting for vehicle measured steering message ...");
+	}
 
-//			}  //namespace autoware
-//		} //namespace motion
-//	} //namespace control
-//} // namespace observer
+} // namespace autoware::motion::control::observer
 
 #include "rclcpp_components/register_node_macro.hpp"
 
-RCLCPP_COMPONENTS_REGISTER_NODE(CommunicationDelayCompensatorNode)
+RCLCPP_COMPONENTS_REGISTER_NODE(autoware::motion::control::observer::CommunicationDelayCompensatorNode)
