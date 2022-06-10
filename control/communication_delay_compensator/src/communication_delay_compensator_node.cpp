67db2a01522f6ea43bf182d412d59e63c1f1db17
ali <boyali@gmail.com>
2022-06-10 17:15:33 +0900

    Reset to last working commit.

--- control/communication_delay_compensator/src/communication_delay_compensator_node.cpp
+++ control/communication_delay_compensator/src/communication_delay_compensator_node.cpp
@@ -27,7 +27,7 @@ namespace observers
 			// 			params_.wheel_base = vehicle_info.wheel_base_m;
 
 			/* set up ros system */
-			float64_t duration{ 0.03 };  // ms
+			float64_t duration{ 50 };  // ms
 			initTimer(duration);
 
 			// Create Publishers
@@ -55,7 +55,7 @@ namespace observers
 
 		void CommunicationDelayCompensatorNode::initTimer(float64_t period_s)
 		{
-			const auto period_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
+			const auto period_ns = std::chrono::duration_cast<std::chrono::milliseconds>(
 				std::chrono::duration<float64_t>(period_s));
 
 			timer_ = rclcpp::create_timer(
